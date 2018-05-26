ab753c7592fab07d8e97259a0a391de7f776071e
Somasundaram M <advaitham@gmail.com>
2018-05-26 18:35:32 +0530

    Auto Connect made module

--- BETA/ConnectKite.py
+++ BETA/ConnectKite.py
@@ -1,4 +1,4 @@
-import time
+import os, sys, time
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -10,65 +10,107 @@ from selenium.common.exceptions import TimeoutException
 
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(1024, 768))
-display.start()
+MY_MOD_PATH="%s" % os.getcwd()
+sys.path.append(MY_MOD_PATH)
 
-#browser = webdriver.Firefox(capabilities=cap, executable_path="C:\\path\\to\\geckodriver.exe")
-#browser.implicitly_wait(100)
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = False
-browser.maximize_window()
-browser.get('https://kite.zerodha.com')
+from credentials import MyUsername, MyPassword, MyQuestions, MyAnswers
 
-#print browser.find_element_by_class_name("login-form")
-#print browser.find_element_by_class_name("uppercase su-input-group")
+def main():
+    #print MyUsername
+    #print MyPassword
+    #print MyQuestions
+    #print MyAnswers
 
-try:
-    elem = browser.find_element_by_xpath("//input[@type='text']")
-    elem.send_keys(USERNAME)
-except:
-    print 'Username not found'
+    display = Display(visible=0, size=(1024, 768))
+    display.start()
 
-try:
-    elem = browser.find_element_by_xpath("//input[@type='password']")
-    elem.send_keys(PASSWORD + Keys.RETURN)
-except:
-    print 'Password not found'
+    cap = DesiredCapabilities().FIREFOX
+    cap["marionette"] = False
+    browser = webdriver.Firefox(capabilities=cap, executable_path="C:\\path\\to\\geckodriver.exe")
+    browser.maximize_window()
+    #browser.implicitly_wait(100)
+    browser.get('https://kite.zerodha.com')
 
-try:
-    elem = browser.find_element_by_xpath("//button[@type='submit']")
-    #elem.send_keys(Keys.RETURN)
-    elem.click()
-except:
-    print 'Unable to submit the form.'
+    #cap = DesiredCapabilities().FIREFOX
+    #cap["marionette"] = False
+    #browser = webdriver.Firefox(capabilities=cap)
+    ##browser = webdriver.Firefox(capabilities=cap, executable_path="/usr/bin/geckodriver")
+    ##browser = webdriver.Firefox(capabilities=cap, executable_path="C:\\path\\to\\geckodriver.exe")
+    #browser.maximize_window()
+    #browser.get('https://kite.zerodha.com')
 
-#WebDriverWait(browser, 100)
-#WebDriverWait(browser, 100).until(EC.title_contains("Kite at Zerodha - The fast, elegant HTML5 trading platform"))
-while True:
+    browser.save_screenshot('/home/somasm/GITRepo/PythonTrade/BETA/1.png')
+
+    # Enter Username
     try:
-	elem = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'container')))
-	print "Page is ready!"
-	time.sleep(5)
-	browser.save_screenshot('/home/somasm/GITRepo/PythonTrade/BETA/1.png')
-	break
+	elem = browser.find_element_by_xpath("//input[@type='text']")
+	elem.send_keys(USERNAME)
     except:
-	print "Page dint load"
-	time.sleep(5)
-
-exit(0)
-
-#delay = 3
-
-print browser.page_source.encode('utf-8')
-
-for twofa in (0, 1, 2, 3, 4):
+	print 'Unable to find username'
+	return False
+    
+    # Enter Password
     try:
-	elem = browser.find_element_by_xpath("//div[contains(.,'%s'%questions[twofa])]")
-        elem.send_keys(answers[twofa] + Keys.RETURN)
+	elem = browser.find_element_by_xpath("//input[@type='password']")
+	elem.send_keys(PASSWORD + Keys.RETURN)
     except:
-        print questions[twofa], "not-found"
-        continue
+	print 'Unable to enter password'
+	return False
+    
+    # Submit first page/form to move on to next password screen
+    try:
+	elem = browser.find_element_by_xpath("//button[@type='submit']")
+	elem.click()
+    except:
+	print 'Unable to submit the first page/form.'
+	return False
+    
+    # Wait for next page to load
+    wait_loop=1
+    while True:
+	try:
+	    elem = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, 'container')))
+	    time.sleep(5)
+	    browser.save_screenshot('/home/somasm/GITRepo/PythonTrade/BETA/2.png')
+	    break
+	except:
+	    print "Page dint load"
+	    if wait_loop > 5:
+		break
+	    time.sleep(5)
+	    wait_loop+=1
+    
+    # Enter next set of passwords
+    all_elements = browser.find_elements_by_xpath("//*[@type='password']")
+    for e in all_elements:
+	label=e.get_attribute('label')
+	print "LABEL=%s" % label
+	for twofa in (0, 1, 2, 3, 4):
+    	    if questions[twofa] in label:
+		print "FOUND: LABEL", label, "QUESITON=", questions[twofa], "ANSWER=", answers[twofa]
+		e.send_keys(answers[twofa] + Keys.RETURN)
+		break
+    
+    browser.save_screenshot('/home/somasm/GITRepo/PythonTrade/BETA/3.png')
+    
+    # Finally submit the 2nd page/form and complete login phase
+    try:
+	elem = browser.find_element_by_xpath("//button[@type='submit']")
+	elem.click()
+    except:
+	print 'Unable to submit the form.'
+	return False
+    
+    time.sleep(5)
+    browser.save_screenshot('/home/somasm/GITRepo/PythonTrade/BETA/4.png')
+    
+    KiteLink='https://kite.trade/connect/login?api_key=xh7iuhiwsnjwo0mi'
+    browser.get(KiteLink)
+    print(browser.current_url)
+    
+    browser.close()
+    display.stop()
+    browser.quit()
 
-browser.close()
-display.stop()
-browser.quit()
+if __name__ == '__main__':
+    main()
