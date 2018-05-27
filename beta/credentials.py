72333a87a00d8817023f1781ef0697646721784c
smjagan <smjagan@yahoo.com>
2018-05-27 23:00:05 +0530

    Delete credentials.py
    Delete this file

--- beta/credentials.py
+++
@@ -1,9 +0,0 @@
-MyUsername = "XM2398"
-MyPassword = "$group123"
-MyQuestions = ['birth place', 'vehicle registration number', 'colour of your first car', 'floors does your building have', 'email service provider']
-MyAnswers = ['erode', '8020', 'gold', 'four', 'gmail']
-
-API_KEY="xh7iuhiwsnjwo0mi"
-API_SECRET_KEY="5ik99ozioowfajcfgesno4omm6h5cma8"
-
-#print MyUsername
